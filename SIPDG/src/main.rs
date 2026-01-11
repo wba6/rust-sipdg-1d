cf47b9ccc74a4fd4aab65162da7b6dd44533d2d6
William <bradenaey1@gmail.com>
2026-01-11 18:35:45 -0500

    Fix errors in linespace

--- SIPDG/src/main.rs
+++ SIPDG/src/main.rs
@@ -29,13 +29,13 @@ fn main() {
     let num_elements: usize = 20;
 
     // Generate evenly spaced points across the domain
-    let x_interface: Vec<f64> = linespace(domain_a, domain_b, num_elements);
+    let x_interface: Vec<f64> = linespace(domain_a, domain_b, num_elements + 1);
     println!("Evenly spaced points are \n {:?}", x_interface);
 
     // With DG we do not share nodes 
     let n_dof: usize = 2 * num_elements;
     let mut x_dof: Vec<f64> = vec![0.0; n_dof];
-    let mut h_elem: Vec<f64> = vec![0 as f64; num_elements];
+    let mut h_elem: Vec<f64> = vec![0.0; num_elements];
 
     // Fill node coordinates and element sizes
     for i in 0..num_elements {
@@ -44,9 +44,11 @@ fn main() {
         h_elem[i] = x_r - x_l;
 
         // map global DoF indices for this element
-        x_dof[2*i-1] = x_l;
-        x_dof[2*i] = x_r;
+        x_dof[2*i] = x_l;
+        x_dof[2*i+1] = x_r;
     }
+    println!("Elements array \n {:?}", h_elem);
+    println!("x dof is \n {:?}", x_dof);
 
     // -------------------- Assemble system matrix ---------
     
--- math/src/linespace.rs
+++ math/src/linespace.rs
@@ -1,13 +1,25 @@
 
 /// generates a row vector of a specified number of linearly (evenly) spaced points between two endpoints
 pub fn linespace(start: f64, end: f64, num_elements: usize) -> Vec<f64> {
-    assert!(num_elements > 0);
-    // Create a vector or num_elements + 1 due to this being inclusive of the domain
-    let mut result: Vec<f64> = vec![0.0; num_elements + 1];
-    let spacing: f64 = (end - start).abs()/num_elements as f64;
-    for (index, value) in result.iter_mut().enumerate() {
-        *value = start + index as f64 * spacing;
+    assert!(num_elements > 0, "num_elements must be greater than 0");
+
+    // Case for exactly one point (avoids division by zero)
+    if num_elements == 1 {
+        return vec![start];
     }
+
+    let mut result: Vec<f64> = Vec::with_capacity(num_elements);
+    
+    let step = (end - start) / ((num_elements - 1) as f64);
+
+    for i in 0..num_elements {
+        result.push(start + (i as f64 * step));
+    }
+    
+    if let Some(last) = result.last_mut() {
+        *last = end;
+    }
+
     return result;
 }
 
